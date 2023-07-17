a0bced99f428ef05fb1b89109e28f18aa5ad6543
DazB <dazb@alumni.york.ac.uk>
2023-07-17 15:09:16 +0800

    Pretty well working app. Managed to get Gooey to a point where it's okay. Potentially one issue down the line is that there is no way to clear the Gooey console. Will this be an issue?

--- app.py
+++ app.py
@@ -17,10 +17,11 @@ this flag, and if true, will send the Delta Play command.
 
 It does this forever and then we die.
 
+Using Gooey to provide a basic interface
+
 Company: Absolute FX Pte Ltd
 Author: Dariush Bahri
 Application Version: 1
-Date: 13th July 2023
 Python Version: 3.11.4 64-bit
 """
 
@@ -29,45 +30,92 @@ import selectors
 import types
 import threading
 import time
-import re
-import subprocess
 from datetime import datetime
 from gooey import Gooey
 from gooey import GooeyParser
+import configparser
+import os
+import psutil
 
+PROGRAM_NAME = 'Delta Command Utility App v1'
 
-# Get IP of the local machine
-DELTA_IP = socket.gethostbyname(socket.gethostname())
+# Network details
 MEDIASONIC_PORT = 4000
 DELTA_PORT = 23
 
+# Ini config
+INI_CONFIG_PATH = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop') + '\DeltaCommandApp.ini'
+INI_SECTION_NETWORK = 'Network'
+INI_CONTROL_INTERFACE = 'control_network_interface'
+
 # Send play flag. If true, will send command to play to Delta
 send_play = False
+# IP address of Delta Server
+delta_ip = ''
 
-@Gooey(program_name='Delta Command Utility App v1'
-       )
+# Main program
+@Gooey(program_name=PROGRAM_NAME, auto_start=True, disable_stop_button=True, header_show_subtitle=False)
 def main():
-    """Main app function"""
     desc = ("This app translates the incoming RSS commands \n" +
-        "into commands compatible with the 7th Sense Server. \n" +
-        "IT MUST REMAIN RUNNING WHILE THE RIDE IS IN OPERATION. \n" +
-        "IF IT IS CLOSED, THE RSS CANNOT PLAY THE VIDEO \n")
+        "into commands compatible with the 7th Sense Server.")
     parser = GooeyParser(description=desc)
-    verbosity = parser.add_mutually_exclusive_group()
-    verbosity.add_argument('Test',
-                           action="store_true", help="Show more details")
-    verbosity.add_argument('testse',
-                           action="store_true", help="Only output on error")
-    parser.parse_args()
 
-    # Run the ipconfig command and capture the output
-    output = subprocess.check_output(['ipconfig']).decode('utf-8')
-    # Extract the IPv4 address for the Ethernet adapter
-    ip_pattern = r'Ethernet adapter Ethernet:\s+.*?IPv4 Address\. . . . . . . . . . . : (\d+\.\d+\.\d+\.\d+)'
-    match = re.search(ip_pattern, output, re.IGNORECASE | re.DOTALL)
-    if match:
-        DELTA_IP = match.group(1)
+    # Check if have config file and saved interface
+    config = configparser.ConfigParser()
+    config.sections()
 
+    global delta_ip
+
+    # If the INI config file exists, then use that interface
+    if os.path.exists(INI_CONFIG_PATH):
+        try:
+            # All network interfaces
+            dict_interfaces = psutil.net_if_addrs()
+            config.read(INI_CONFIG_PATH)
+            saved_interface = config[INI_SECTION_NETWORK][INI_CONTROL_INTERFACE]
+            delta_ip = dict_interfaces[saved_interface][1].address
+        except:
+            pass
+    
+    # If we don't have a selected interface, then run config GUI
+    if delta_ip == '':
+        # Check if have config file and saved interface
+        config = configparser.ConfigParser()
+        config.sections()
+        # All network interfaces
+        dict_interfaces = psutil.net_if_addrs()
+
+        # Adds network interfaces and IP's to a list
+        network_choices = []
+        for interface in dict_interfaces:
+            network_choices.append(interface + " - IP Address: " + dict_interfaces[interface][1].address)
+        # Add dropdown listbox argument
+        parser.add_argument(
+            'interface',
+            metavar='Control Network Interface',
+            help='Choose the network interface on this machine that connects to the Control Network',
+            type=str,
+            choices=network_choices,
+            gooey_options={'full_width': True})
+        # This will run the GUI. Once arguments selected, will continue
+        args = parser.parse_args()
+        # Write Interface choice to INI file
+        interface_index = args.interface.find('-')
+        interface_name = args.interface[:interface_index - 1]
+        config[INI_SECTION_NETWORK] = {}
+        config[INI_SECTION_NETWORK][INI_CONTROL_INTERFACE] = interface_name
+        with open(INI_CONFIG_PATH, 'w') as configfile:
+            config.write(configfile)
+
+        # Get IP address 
+        ip_index = args.interface.find(':')  # Find the index of the character
+        delta_ip = args.interface[ip_index + 2:] # Get the IP after the character
+    
+    # Else we already have an IP address. Autostart
+    else:
+        parser.parse_args()
+
+    # Main logic for handling the connections and data
     # Handles server multiplexing
     sel = selectors.DefaultSelector()
 
@@ -75,11 +123,11 @@ def main():
     # Create an INET, STREAMing socket
     serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # Bind the server socket to Delta IP, and the Mediasonic port
-    serversocket.bind((DELTA_IP, MEDIASONIC_PORT))
+    serversocket.bind((delta_ip, MEDIASONIC_PORT))
     # Open server socket. Listen up to a max of 2 connections (one for RSS, one for debugging)
     serversocket.listen(2)
     serversocket.setblocking(False)
-    log(f"Listening on {(DELTA_IP, MEDIASONIC_PORT)}")
+    log(f"Listening on {(delta_ip, MEDIASONIC_PORT)}")
     # Register the server to be monitored for read events
     sel.register(serversocket, selectors.EVENT_READ, data=None)
 
@@ -99,10 +147,10 @@ def main():
         log("Caught keyboard interrupt, exiting")
     finally:
         sel.close()
-        
 
+        
+# Handles incoming server messages
 def service_connection(key, mask, sel: selectors.DefaultSelector):
-    """ Handles incoming server messages """
     global send_play
     sock: socket.socket = key.fileobj
     data = key.data
@@ -130,9 +178,9 @@ def service_connection(key, mask, sel: selectors.DefaultSelector):
             elif 'TCSTART 1!' in recv_data:
                 log("Received timecode start command")
                 send_play = True
-            
+    
+# Handles client connection to the Delta app
 def client_thread_function():
-    """ Handles client connection to the Delta app """
     global send_play
     while True:
         try: 
@@ -143,10 +191,10 @@ def client_thread_function():
             while True:
                 if send_play:
                     send_play = False
-                    log("Sending play command to Delta")
                     try:
                         # Send Delta Play command
                         clientsocket.send(b'Play')  
+                        log("Sent play command to Delta")
                     except:
                         log("Error sending play command. Delta Server not running? Will attempt reconnect")
                         break
@@ -155,18 +203,15 @@ def client_thread_function():
             # Connection was refused, wait for some time before retrying
             time.sleep(5)
 
-
+# Log utility function
 def log(message: str):
-    # Log utility function
     # Get the current date and time
     current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     # Print the output with the current time and date
     print(f"[{current_time}] {message}")
 
-
 if __name__ == "__main__":
     main()
     log("Delta Command Utility App closing. COMMANDS FROM RSS WILL NO LONGER BE ABLE TO CONTROL THE DELTA SERVER. \n" + 
-        "If unintentional, re-run the app (python app.py in command line), or restart the server. \n" + 
+        "If unintentional, re-run the app or restart the server. \n" + 
         "BYE")
-    
